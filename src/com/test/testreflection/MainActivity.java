14c0a6580ce6a6976272ad6e684019a32feb2a3a
SanghoSeo <ssh0702@gmail.com>
2015-03-25 15:34:51 +0900

    Update Example MainActivity

--- src/com/test/testreflection/MainActivity.java
+++ src/com/test/testreflection/MainActivity.java
@@ -1,16 +1,23 @@
 package com.test.testreflection;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.digicap.android.reflection.Reflection;
 import com.digicap.android.reflection.Reflection.SecurityLevel;
+import com.digicap.android.reflection.listener.CpKeyListener;
 import com.digicap.android.reflection.ReflectionListener;
 
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ReflectionListener {
     
     private static final String LOG_TAG = "ReflectionJar";
 
@@ -32,27 +39,31 @@ public class MainActivity extends Activity {
         String test_pkg_name_1 = "com.digicap.android.test30";
         
         
+        // Read Init Certificate, Init Key
+        byte[] mCertData = null;
+        byte[] mKeyData = null;
+        try {
+            AssetManager assetMgr = getAssets();
+            InputStream is = assetMgr.open("init.pem");
+            mCertData = IOUtils.toByteArray(is);
+            
+            is = assetMgr.open("init.key");
+            mKeyData = IOUtils.toByteArray(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
         // 1. Get Reflection Instance
         Reflection mReflection = Reflection.getInstance(this.getApplication());
         
         
         // 2. Register Listener
-        mReflection.registerDevice(new ReflectionListener() {
+        mReflection.registerDevice(this, mCertData, mKeyData);
 
-            @Override
-            public void updateSecurityLevel(String pkg_name, SecurityLevel level) {
-                Log.d(LOG_TAG, "RegisterDevice(). Package Name is " + pkg_name + ". Level is " + level);                
-            }
-        });
 
         // 3. Add PackageName
-        mReflection.addAppPackage(test_pkg_name_1, new ReflectionListener() {
-
-            @Override
-            public void updateSecurityLevel(String pkg_name, SecurityLevel level) {
-                Log.d(LOG_TAG, "addAppPackage(). Package is " + pkg_name + ". SecurityLevel is " + level);   
-            }
-        });
+        mReflection.addAppPackage(test_pkg_name_1, this);
         
         
         // 4. Get SecurityLevel of Companion App 
@@ -64,8 +75,31 @@ public class MainActivity extends Activity {
         SecurityLevel cpLevel = mReflection.getSecurityLevel(test_pkg_name_1);
         Log.d(LOG_TAG, "CP SecurityLevel is " + cpLevel);
         
+        
+        // 6. Get CP Key API
+        mReflection.getCPKey(new CpKeyListener() {
+            
+            @Override
+            public void resultInfo(int resultCode, String descrition, byte[] cpKeyData) {
+                if (cpKeyData != null) {
+                    Log.d(LOG_TAG, "ResultCode is " + resultCode + ". Description is " + descrition + ". Cp Key is " + new String(cpKeyData));
+                } else {
+                    Log.d(LOG_TAG, "ResultCode is " + resultCode + ". Description is " + descrition + ". Cp Key is null");
+                }
+            }
+        });
         ///////////////////////////////////////////////////////////////////////
-        
-        
+    }
+
+
+    @Override
+    public void updateRootingStauts(SecurityLevel level) {
+        Log.d(LOG_TAG, "updateRootingStauts(). Rooting is " + level);        
+    }
+
+
+    @Override
+    public void updateSecurityLevel(String pkg_name, SecurityLevel level) {
+        Log.d(LOG_TAG, "updateSecurityLevel(). Package Name is " + pkg_name + ". Level is " + level);
     }
 }
